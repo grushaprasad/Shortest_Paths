9ff3dd4c0befad36a58a87dbdfd6970ec68c2bef
grushaprasad <grushaprasad@greenwichwireless161-215.hampshire.edu>
2015-12-10 20:36:08 -0500

    K^2 log K implementation of shortest path of k queries
    This version takes advantage of the grid structure. It
    1. Runs dijkstras on only all the corners in each cell
    2. Uses this to maintain a corner_edges list
    3. For each query, creates a subset list of vertices from source cell,
    vertices from the destination cell and all the corners
    4. Runs dijkstras on this subset and prints out the path

--- src/DijktrasAlgorithm.java
+++ src/DijktrasAlgorithm.java
@@ -1,32 +1,25 @@
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.*;
 
 public class DijktrasAlgorithm {
 	
-	private Map<String, ArrayList<Map.Entry<String, Integer>>> vertexList;
+	private Map<String, HashMap<String, Node>> vertexList;
 	private String source;
 	private HashMap<String, Node> map;
 	private Set<String> keySet;
 
-	public DijktrasAlgorithm(Map<String, ArrayList<Map.Entry<String, Integer>>> v, String s) {		
+	public DijktrasAlgorithm(Map<String, HashMap<String, Node>> v, String s) {	 
 		vertexList = v;
 		source = s;
-		//System.out.println(source);
 		
-		map= new HashMap<String, Node>();
-		map.put(source, new Node(0, null, source));
-		//System.out.println("map.get(source).getPrioirty() = " + map.get(source).getPriority());
+		map= new HashMap<String, Node>();  //This is the map that will be returned
 		keySet = vertexList.keySet();
 		
 		//initializing all nodes to infinity, all previous to null. Then sets source priority to 0
 		for (String str: keySet) {
-			 //System.out.println(str + " is the string in the for loop right now");
 			 if (str.equals(source)) {
-				 map.put(str, new Node(0, null, str));
-				 //System.out.println("Reached if statement");
+				 map.put(str, new Node(0, null, str, new ArrayList<String>()));
 			 }
-			 else map.put(str, new Node(Integer.MAX_VALUE, null, str));
+			 else map.put(str, new Node(Integer.MAX_VALUE, null, str, new ArrayList<String>()));
 		}
 		
 	}
@@ -35,97 +28,78 @@ public class DijktrasAlgorithm {
 
 	public HashMap<String, Node> runDijkstras() {
 		PriorityQueue<String, Integer> q = new PriorityQueue<String,Integer>();
-		
-		
-		//System.out.println("Printing vertex list");
-		/*for (Map.Entry<String, ArrayList<Map.Entry<String, Integer>>> entry : vertexList.entrySet()) {
-	    System.out.println(entry.getKey()+" : "+entry.getValue());
-	} 
-		
-		System.out.println();
-		System.out.println();
-		System.out.println(); */
-		
-		
+
 		// Add all the vertices to the priority queue.
 		for (Map.Entry<String, Node> entry : map.entrySet()) {
-			//System.out.println("entry is" + entry.getKey() + " "+ entry.getValue().getPriority() + entry.getValue().getPrevious());
-			//System.out.println("q.getSize() was " + q.getSize());
 		    q.addItem(entry.getKey(), entry.getValue().getPriority());
-		    //System.out.println("q.getSize() is after the add " + q.getSize());
-		   //System.out.println(q.toString());
-		   //System.out.println();
 		}
-		
-		//System.out.println(q.toString());
+
 		
 		//visit all vertices in the priority queue
-		while(q.getSize() > 0) {
-			//System.out.println("starting while loop");
+		while(q.getSize() > 0) {	
 			int priority_u = q.peekPriority();
-			//System.out.println("priority_u the first time it is set is " + priority_u);
 			String minVertex = q.removeItem();
-			ArrayList<Map.Entry<String, Integer>> u = vertexList.get(minVertex); //the adjacency list of current node with lowest prioirty
+			HashMap<String, Node> u = vertexList.get(minVertex); //the adjacency list of current node with lowest prioirty
 		
-			//System.out.println("Printing keys in adjacency list");
-			for (Map.Entry<String, Integer> entry : u) {
-				//System.out.println(entry.getKey()+" : "+entry.getValue());
-			}			
-			//System.out.println("Done printing");
-			//System.out.println();
-			//iterate through the adjacency list to see if any of these nodes have lesser weight from source now.
-			for (int v = 0; v < u.size(); v++) {
-				String vertex = u.get(v).getKey();    
-				int priority_v = map.get(vertex).getPriority();
-				int dist_u_to_v = u.get(v).getValue();
-				if(priority_u + dist_u_to_v < priority_v) {   //if the priority (shortest distance to u) of u + weight of v (which is distance of u to v) < priority (shortest distance to v) of v
-					//System.out.println("priority_v was " + priority_v);
-					map.get(vertex).setPriority(priority_u + dist_u_to_v);  //alters the distance in HashMap
-					map.get(vertex).setPrevious(map.get(minVertex));   //Sets predecessor. map.get(minVertex) gives the node of the string (of our current u) in our HashMap
-					q.decreasePriority(vertex, priority_u + dist_u_to_v); //changes priority in the P.Q.
+			
+			for (Map.Entry<String, Node> entry : u.entrySet()) {
+				String vertex = entry.getKey();  	
+				if(map.get(vertex) != null) { //if map has the curr vertex from the adjacecny list
+					int priority_v = map.get(vertex).getPriority();
+					int dist_u_to_v = entry.getValue().getPriority();
+					if(priority_u + dist_u_to_v < priority_v) {   //if the priority (shortest distance to u) of u + weight of v (which is distance of u to v) < priority (shortest distance to v) of v
+						map.get(vertex).setPriority(priority_u + dist_u_to_v);  //alters the distance in HashMap
+						map.get(vertex).setPrevious(map.get(minVertex));   //Sets predecessor. map.get(minVertex) gives the node of the string (of our current u) in our HashMap
+						q.changePriority(vertex, priority_u + dist_u_to_v); //changes priority in the P.Q.
+					}
 				}
 			}
 		}
-		/*System.out.println("Printing map");
-		for (Map.Entry<String, Node> entry : map.entrySet()) {
-			System.out.println(entry.getKey()+" : "+entry.getValue());
-		}
-		System.out.println(); */
 		
 		return map;
 	}
 	
-	public ArrayList<Map.Entry<Integer,ArrayList<String>>> allPaths() {
-		ArrayList<Map.Entry<Integer,ArrayList<String>>> allPaths = new ArrayList<Map.Entry<Integer,ArrayList<String>>>();
-		HashMap<String, Node> x = runDijkstras();
-		//iterating through the hashmap
-		for (Map.Entry<String, Node> entry : x.entrySet()) { 
-			int weight = entry.getValue().getPriority();
-		    ArrayList<String> str = new ArrayList<String>();
-		    str.add(entry.getKey());
-		    Node curr = entry.getValue();
-		    while (curr.previous != null) {
-		    	String previous = curr.previous.getVertex();
-		    	str.add(previous);
-		    	curr = curr.previous;
-		    }
-		    
-		    allPaths.add(new AbstractMap.SimpleEntry<Integer, ArrayList<String>>(weight, str));
-		}
-		return allPaths;
-	}
 	
-	public Map.Entry<Integer,ArrayList<String>> source_to_destination(String vertex) {
-		HashMap<String, Node> x = runDijkstras();
+	public Map.Entry<Integer,ArrayList<String>> source_to_destination(String vertex, HashMap<String, HashMap<String, Node>> corner_edges) {
+		HashMap<String, Node> x = runDijkstras(); //O(K log K)
 		Node destination = x.get(vertex);
 		int weight = destination.getPriority();
 		ArrayList<String> str = new ArrayList<String>();
 	    str.add(vertex);
 	    Node curr = destination;
-	    while (curr.previous != null) {
+	    while (curr.previous != null) {  //O(K)
 	    	String previous = curr.previous.getVertex();
-	    	str.add(previous);
-	    	curr = curr.previous;
+	    	if (previous.charAt(0) == 'g') { //if it is a corner 
+	    		if(curr.previous.previous.getVertex().charAt(0) =='g') {// if the one previous to that is also a corner
+	    			/*
+	    			 * If both current and the previous are corners, we want to go to the curr in corner_edges
+	    			 * Then find curr.previous in the adjacecny list and add all the elements in the path to the array
+	    			 */
+	    			Node a = curr.previous;
+	    			Node b = curr.previous.previous;
+	    			String b_vertex = b.getVertex();
+	    			String a_vertex = a.getVertex();
+	    			ArrayList<String> path = corner_edges.get(b_vertex).get(a_vertex).getPath();
+	    			if(path.size() > 1) { //only if there is more than one node in the path
+	    				path.remove(path.size() - 1); //because we don't want to add the source, since it will get added whencurr.previous gets set to curr and curr in printed
+		    			for(String s: path) {
+		    				str.add(s);
+		    			}
+	    			}
+	    			
+	    			curr = curr.previous;
+	    		}	
+	    		
+	    		else {//the one previous to that is not a corner
+	    			str.add(previous);
+	    	    	curr = curr.previous;
+	    		}
+	    	}
+	    	
+	    	else { //if it is not a corner
+	    		str.add(previous);
+		    	curr = curr.previous;
+	    	} 	
 	    }
 	    
 	    return new AbstractMap.SimpleEntry<Integer,ArrayList<String>>(weight, str);
@@ -133,38 +107,48 @@ public class DijktrasAlgorithm {
 	
 
 	public static class Node {
-		private Node previous;
-		private int priority;
-		private String vertex;
+		Node previous;
+		int priority;
+		String vertex;
+		ArrayList<String> path;
 		
-		private Node (int i, Node prev, String str) {
+		 Node (int i, Node prev, String str, ArrayList<String> p) {
 			priority = i;
 			previous = prev;
 			vertex = str;
+			path = p;
 		}
 		
-		private void setPriority(int p) {
+		ArrayList<String> getPath() {
+			return path;
+		}
+		void setPriority(int p) {
 			priority = p;
 		}
 		
-		private void setPrevious(Node prev) {
+		void setPrevious(Node prev) {
 			previous = prev;
 		}
 		
-		private Node getPrevious() {
+		Node getPrevious() {
 			return previous;
 		}
 		
-		private String getVertex() {
+		String getVertex() {
 			return vertex;
 		}
 		
-		private int getPriority() {
+		int getPriority() {
 			return priority;
 		}
 		
+		void setPath(ArrayList<String> x) {
+			path = x;
+		}
+
+		
 		public String toString() {
-			String str =  "[" + vertex + ", " + priority + "]";
+			String str =  "[" + vertex + ", " + priority + ", " + path + "]";
 			return str;
 		}
 	}
--- src/ShortestPathsMain.java
+++ src/ShortestPathsMain.java
@@ -1,104 +1,267 @@
 
 import java.util.*;
+import java.lang.management.*; 
 
-//import DijktrasAlgorithm.Node;
-
-import java.awt.geom.Point2D;
 
 public class ShortestPathsMain {
 
 	public static void main(String[] args) {
-		
+		ThreadMXBean bean = ManagementFactory.getThreadMXBean();
 		Scanner scan = new Scanner(System.in);
+		long t = bean.getCurrentThreadUserTime();
 		int width = scan.nextInt();
 		int height = scan.nextInt();
 		int number = scan.nextInt();
 		ArrayList<String> source = new ArrayList<String>();
 		ArrayList<String> destination = new ArrayList<String>();
 		
-		Map<String, ArrayList<Map.Entry<String, Integer>>> vertexList = new HashMap <String, ArrayList<Map.Entry<String, Integer>>>(); //Make it an array list of nodes!!
+		/*Making an Array List of all vertices and adjacency list of these vertices for all cells:
+		 * Each cell: HashMap<vertex, adjacency list>
+		 * Each adjacency list: HashMap<vertex, Node>
+		 */
+		ArrayList<HashMap <String, HashMap<String, DijktrasAlgorithm.Node>>> List_of_cells = new ArrayList<HashMap <String, HashMap<String, DijktrasAlgorithm.Node>>>();
+		
+		HashMap<String, HashMap<String, DijktrasAlgorithm.Node>> vertexList;
+		
+		//Add all the possible vertices to the vertexList.
 		for (int i = 0; i < height; i++) {
+			//beginning of i loop, all the vertexLists for all values of j for vi-1.j have been added
 			for (int j = 0; j < width; j++){
-				// make random graph within grid square (i,j)
-				//List<String> verts = new ArrayList<String>();
+				//Beginning of loop, vertexList initialized to null;
+				 vertexList = new HashMap<String, HashMap<String, DijktrasAlgorithm.Node>>();
+				// make the rest
+				for (int v = 0; v < number; v++){
+					//Adds all the vertices with vi.j.__
+					vertexList.put(("v" + i + "." + j + "." + v), new HashMap<String, DijktrasAlgorithm.Node>());
+				    }
 				
 				// make the corners
-				for (int di = 0; di <= 1; di++){
-					for (int dj = 0; dj <= 1; dj++){
-						vertexList.put(("g" + (i + di) + "." + (j + dj)), new ArrayList<Map.Entry<String, Integer>>());
+				for (int di = i; di <= i+1; di++){
+					for (int dj = j; dj <=j+1; dj++){
+						vertexList.put(("g" + (di) + "." + (dj)), new HashMap<String, DijktrasAlgorithm.Node>());
 					    }
-				    }
-	
-				// make the rest
-				for (int v = 0; v < number; v++){
-					vertexList.put(("v" + i + "." + j + "." + v), new ArrayList<Map.Entry<String, Integer>>());
-				    }
-	
-				// make the rest
-				for (int v = 0; v < number; v++){
-					vertexList.put(("v" + i + "." + j + "." + v), new ArrayList<Map.Entry<String, Integer>>());
-					}
+				    } 
+				List_of_cells.add(vertexList); //it adds all vertices with the vi.j*. Where j* is current value of j 
 			}
+			//end of loop  all the vertexLists for all values of j for vi.j have been added
 	    }
-
 		
+		
+		//Making a list of all corners
+
+		ArrayList<String> corner_list = new ArrayList<String>();
+		
+		for (int i = 0; i < height+1; i++) {
+			for (int j = 0; j < width+1; j++){
+				corner_list.add("g" + i + "." + j);
+					}
+				} 
+	
+		//Taking in input and adding all the vertices and their edges to all the vertices in all cells.
 		while (scan.hasNextLine()) {
 			String next = scan.nextLine(); 
 			
-			//System.out.println(next);
-			
-				String [] x = next.split(" ", 3);
-				if (x.length == 3) {  // just so it stops at the line with queries
+				String [] x = next.split(" ", 3); //Splits the input by space. Divides it into at most 3 parts.
+				
+				if (x.length == 3) {  // If it is a vertex and *not* a query
+					int weight = Integer.valueOf(x[2]).intValue();
 					String u = x[0];
 					String v = x[1];
-					int weight = Integer.valueOf(x[2]).intValue();
-					vertexList.get(u).add(new AbstractMap.SimpleEntry<String, Integer>(v, weight));	
-					vertexList.get(v).add(new AbstractMap.SimpleEntry<String, Integer>(u, weight));
-					System.out.println("if (x.length == 3)"
-							+ " ended");
+					String [] split_u = u.split("[.]"); //split by '.'
+					String [] split_v = v.split("[.]");
+					split_u[0]= split_u[0].substring(1); //remove the first character from the first element in the array because this is a character
+					split_v[0]= split_v[0].substring(1);
+					
+					
+					if (split_u.length == 3 && split_v.length == 3) { // i.e neither is a corner
+						int u_i = Integer.valueOf(split_u[0]).intValue();
+						int u_j = Integer.valueOf(split_u[1]).intValue();
+						int cell = (width * u_i) + u_j;
+						List_of_cells.get(cell).get(u).put(v, new DijktrasAlgorithm.Node(weight, null, v, new ArrayList<String>()));						
+						List_of_cells.get(cell).get(v).put(u, new DijktrasAlgorithm.Node(weight, null, u, new ArrayList<String>()));			
+					}
+					
+					else if (split_u.length == 3) { //i.e. v is a corner
+						int u_i = Integer.valueOf(split_u[0]).intValue();
+						int u_j = Integer.valueOf(split_u[1]).intValue();
+						int v_i = Integer.valueOf(split_v[0]).intValue();
+						int v_j = Integer.valueOf(split_v[1]).intValue();
+						
+						if (v_i >= height) {
+							v_i = v_i-1;
+						}
+						
+						if (v_j >= width) {
+							v_j = v_j-1;
+						}
+						
+						int cell_u = (width * u_i) + u_j;
+						int cell_v = (width * v_i) + v_j; //note for purposes of this assignment this will be equal to cell_u. But this adds clarity and is more generalizable
+						
+						//Adding v to u's adjacency list
+						List_of_cells.get(cell_u).get(u).put(v, new DijktrasAlgorithm.Node(weight, null, v, new ArrayList<String>()));
+						List_of_cells.get(cell_u).get(v).put(u, new DijktrasAlgorithm.Node(weight, null, u, new ArrayList<String>()));
+					}
+					
+					else if (split_v.length == 3) { //i.e u is a corner
+						int u_i = Integer.valueOf(split_u[0]).intValue();
+						int u_j = Integer.valueOf(split_u[1]).intValue();
+						int v_i = Integer.valueOf(split_v[0]).intValue();
+						int v_j = Integer.valueOf(split_v[1]).intValue();
+						
+						if (u_i >= height) {
+							u_i = u_i-1;
+						}
+						
+						if (u_j >= width) {
+							u_j = u_j-1;
+						}
+						
+						int cell_u = (width * u_i) + u_j;
+						int cell_v = (width * v_i) + v_j; //note for purposes of this assignment this will be equal to cell_u. But this adds clarity and is more generalizable
+						
+						//Adding u to v's adjacency list
+						List_of_cells.get(cell_v).get(v).put(u, new DijktrasAlgorithm.Node(weight, null, u, new ArrayList<String>()));
+						List_of_cells.get(cell_v).get(u).put(v, new DijktrasAlgorithm.Node(weight, null, v, new ArrayList<String>()));						
+					}  
 				}
 				
-				if (x.length == 2) {
+				if (x.length == 2) {  //this is query
 					source.add(x[0]);
 					destination.add(x[1]);
-					System.out.println("if (x.length == 2)"
-							+ " ended");
 				}
 				
 		}
 		
-		System.out.println("Source at 1 is " + source.get(0));
 		
+		//Creating corner_edges: A list of all the corners and the adjacency lists with all other corners
+		HashMap<String, HashMap<String, DijktrasAlgorithm.Node>> corner_edges = new HashMap<String, HashMap<String, DijktrasAlgorithm.Node>>();
 		
-		
-		//prints the adjacency list! Code taken from: http://stackoverflow.com/questions/5920135/printing-hashmap-in-java
-		
-		
-		
-		ArrayList<ArrayList<Map.Entry<Integer,ArrayList<String>>>> solvedList = new ArrayList< ArrayList<Map.Entry<Integer,ArrayList<String>>>>();
-		
-		/*for (int i = 0; i< source.size(); i++) {
-			DijktrasAlgorithm a = new DijktrasAlgorithm(vertexList, source.get(i));
-			solvedList.add(a.allPaths());
-		} */
-		
-		/*for (Map.Entry<String, ArrayList<Map.Entry<String, Integer>>> entry : vertexList.entrySet()) {
-	    System.out.println(entry.getKey()+" : "+entry.getValue());
-	}*/
-		
-		/*for (Map.Entry<Integer,ArrayList<String>> entry : solvedList.get(0)) {
-			System.out.println(entry.getKey()+" : "+entry.getValue());
-		} */
-		
-		for (int i = 0; i< source.size(); i++){
-			DijktrasAlgorithm abc = new DijktrasAlgorithm(vertexList, source.get(i));
-			Map.Entry<Integer,ArrayList<String>> dest = abc.source_to_destination(destination.get(i));
-			Collections.reverse(dest.getValue());
-			System.out.println(dest.getKey() + " " + dest.getValue());
-		}
-		
-		scan.close();
 
+		for(HashMap<String, HashMap<String, DijktrasAlgorithm.Node>> cell: List_of_cells) { //for each cell O(V)
+			ArrayList<String> corners = new ArrayList<String>();
+			for(Map.Entry<String, HashMap<String, DijktrasAlgorithm.Node>> vertex: cell.entrySet()) { //for each vertex O(V)
+				if (vertex.getKey().charAt(0) == 'g') { //if the vertex is a corner
+					corners.add(vertex.getKey());
+				}
+			}
+			
+			for(String corner: corners) { //O(1)
+				DijktrasAlgorithm x = new DijktrasAlgorithm(cell, corner);
+				HashMap<String, DijktrasAlgorithm.Node> dijktras_result = x.runDijkstras();
+				for(String corner2: corners) { //O(1)
+					ArrayList<String> adjCorners = new ArrayList<String>();
+					adjCorners.add(corner2);
+				    DijktrasAlgorithm.Node curr = dijktras_result.get(corner2);
+				    while (curr.previous != null) {  //O(k)
+				    	String previous = curr.previous.getVertex();
+				    	adjCorners.add(previous);
+				    	curr = curr.previous;
+				    }
+				    
+				    
+				    //Adding corner2 to corner in corner_edges
+				    if (corner_edges.get(corner) != null) { //corner_edges has the curr corner
+						if (corner_edges.get(corner).get(corner2) != null) { //the curr corner has the curr vertex in the adjacency list of corner_edges
+							if (corner_edges.get(corner).get(corner2).getPriority() > dijktras_result.get(corner2).getPriority()) { //value in corner_edges is greater than the value in dijkstras_result
+								corner_edges.get(corner).get(corner2).setPriority(dijktras_result.get(corner2).getPriority());
+							}
+						}
+						
+						else { //curr corner doesn't have curr vertex
+							corner_edges.get(corner).put(corner2, dijktras_result.get(corner2)); //add
+						}
+					}
+					
+					else { //corner_edges doesn't have curr corner.
+						corner_edges.put(corner, new HashMap<String, DijktrasAlgorithm.Node>()); //add corner
+						corner_edges.get(corner).put(corner2, dijktras_result.get(corner2));//add vertex to corner			
+					}
+				    
+				    //Adding the path to corner2
+				    
+				    corner_edges.get(corner).get(corner2).setPath(adjCorners);
+				}
+			}
+				
+		}
+
+		
+		for (int i = 0; i< source.size(); i++) {  //for each source (in other words each query)
+			HashMap<String, HashMap<String, DijktrasAlgorithm.Node>> subsetList = new HashMap<String, HashMap<String, DijktrasAlgorithm.Node>>();
+			String s = source.get(i);
+			String [] split_source = s.split("[.]");
+			
+			//find out the cell of the source
+			int source_i = Integer.valueOf(split_source[0].substring(1));
+			int source_j = Integer.valueOf(split_source[1]).intValue();
+			int cell_source = (width * source_i) + source_j;
+
+			
+			String d = destination.get(i);
+			String [] split_dest = d.split("[.]");
+			
+			//find out the cell of the destination
+			int dest_i = Integer.valueOf(split_dest[0].substring(1));
+			int dest_j = Integer.valueOf(split_dest[1]).intValue();
+			int cell_dest = (width * dest_i) + dest_j;
+			
+			HashMap <String, HashMap<String, DijktrasAlgorithm.Node>> sourceCell = List_of_cells.get(cell_source);
+			HashMap <String, HashMap<String, DijktrasAlgorithm.Node>> destCell = List_of_cells.get(cell_dest);
+			
+			//ADD RELEVANT NODES IN SUBSET LIST
+			
+			//put in corner edges
+			for(Map.Entry<String, HashMap<String, DijktrasAlgorithm.Node>> entry: corner_edges.entrySet()) { // O(V)
+				HashMap<String, DijktrasAlgorithm.Node> x = new HashMap<String, DijktrasAlgorithm.Node>();
+				for(Map.Entry<String, DijktrasAlgorithm.Node> y: entry.getValue().entrySet()) { //O(V)
+					x.put(y.getKey(), y.getValue());
+				}
+				subsetList.put(entry.getKey(), x);
+			}
+			
+			// put in the edges to vertices in corner edges from source and destination cells
+			for (int m = 0; m < corner_list.size(); m++) { //O(V)
+				if(sourceCell.containsKey(corner_list.get(m))) {  //if the source cell has that corner
+					for (Map.Entry<String, DijktrasAlgorithm.Node> entry: sourceCell.get(corner_list.get(m)).entrySet()) { //for each vertex in adjacency list of the corner
+						subsetList.get(corner_list.get(m)).put(entry.getKey(), entry.getValue());
+					}
+				}
+				
+				if(destCell.containsKey(corner_list.get(m))) {  //if the destination cell has that corner
+					for (Map.Entry<String, DijktrasAlgorithm.Node> entry: destCell.get(corner_list.get(m)).entrySet()) { //for each vertex in adjacency list of the corner
+						subsetList.get(corner_list.get(m)).put(entry.getKey(), entry.getValue());
+					}
+				}
+				
+			}
+			
+			//add in vertices from source cell to the subset
+			
+			for(Map.Entry<String, HashMap<String, DijktrasAlgorithm.Node>> entry: sourceCell.entrySet()) {
+				if (entry.getKey().charAt(0) != 'g') {//to make sure we are not adding corners
+					subsetList.put(entry.getKey(), entry.getValue());
+				}
+			}
+			
+			//add in vertices from destination cell to the subset
+			
+			for(Map.Entry<String, HashMap<String, DijktrasAlgorithm.Node>> entry: destCell.entrySet()) {
+				if (entry.getKey().charAt(0) != 'g') { //to make sure we are not adding corners
+					subsetList.put(entry.getKey(), entry.getValue());
+				}
+			}
+			
+			//Run dijktras on the subset list
+			DijktrasAlgorithm sp = new DijktrasAlgorithm(subsetList, s); 
+			Map.Entry<Integer,ArrayList<String>> dest = sp.source_to_destination(destination.get(i), corner_edges);
+			Collections.reverse(dest.getValue());
+			System.out.println(dest.getKey() + " " + dest.getValue()); 			
+		} 
+		
+		System.out.printf ("Time take was %f seconds.\n",    
+			       (bean.getCurrentThreadUserTime()-t) / 1e9);
+		scan.close();
 	}
 }
 	
